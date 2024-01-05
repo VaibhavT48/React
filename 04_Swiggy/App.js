2bc4c937997d87afb47a343d2112fa39808dfd54
vaibhav <vaibhavyadav4884@gmail.com>
2024-01-05 21:34:25 +0530

    Reaconcilation, Fiber, Real DOM

--- 04_Swiggy/App.js
+++ 04_Swiggy/App.js
@@ -1,7 +1,7 @@
 
-
-import React, { Fragment } from "react";
-import  ReactDOM from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import FoodFireLogo from "./Images/Food Fire Logo.png";
 
 /* My Food App structure will look like this, 
             1) Header
@@ -21,53 +21,32 @@ import  ReactDOM from "react-dom/client";
        
 */
 
-  
-  // Header component for header section: Logo, Nav Items
-  const Header = () => {
-    return (
-      <div className="header">
-        <div>
-            <img className = "logo"src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"/>
-        </div>
-        <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            
-          </ul>
-        </div>
+// Title component for display logo
+const Title = () => (
+  <a href="/">
+    <img className="logo" src={FoodFireLogo} alt="Food Fire Logo" />
+  </a>
+);
+
+// Header component for header section: Logo, Nav Items
+const Header = () => {
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li><i class="fa-solid fa-cart-shopping"></i></li>
+        </ul>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-  
-
-  const RestaurantCard = (props) => {
-    const{resData} = props
-
-    const {name, cuisines, avgRating, deliveryTime} = resData.data
-    return(
-        <div className="res-card">
-             <img className="logo"
-       alt="res-logo" src={
-        "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +  "cloudinaryImageId"}
-       /> 
-       <h3>{name}</h3>
-       <h4>{cuisines}</h4>
-       <h4>{avgRating}Stars</h4>
-       <h4>{deliveryTime}Minutes</h4>
-       </div>
-  
-      
-
-    );
-  };
-
-
-  
 // RestaurantList is JSON Data for displaying cards
-const RestaurantList = [
+const restaurantList = [
   {
     type: "restaurant",
     data: {
@@ -1885,41 +1864,75 @@ const RestaurantList = [
   },
 ];
 
+// Restaurant card component: Image, name, cuisine
+const RestaurantCard = ({
+  cloudinaryImageId,
+  name,
+  cuisines,
+  area,
+  lastMileTravelString,
+  costForTwoString,
+  avgRating,
+}) => {
+  return (
+    <div className="card">
+      <img
+        src={
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
+          cloudinaryImageId
+        }
+      />
+      <h2>{name}</h2>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{area}</h4>
+      <span>
+      <h4><i class="fa-solid fa-star"></i>{avgRating}</h4>
+        <h4>{lastMileTravelString}</h4>
+        <h4>{costForTwoString}</h4>
+      </span>
+    </div>
+  );
+};
 
-  const Body = () => {
-    return(
-           <div className="body">
-             <div className="Search"> Search</div>
-            <div className="res-container">
-            {
-              RestaurantList.map((restaurant) => {
-                <RestaurantCard key={restaurant.data.id} resData={restaurant} />
-              })
+// Body Component for body section: It contain all restaurant cards
+// We are mapping restaurantList array and passing data to RestaurantCard component as props with unique key as index
+const Body = () => {
+  return (
+    <div className="restaurant-list">
+      {restaurantList.map((restaurant) => {
+        return <RestaurantCard key={restaurant.data.id} {...restaurant.data} />;
+      })}
+    </div>
+  );
+};
 
-            }
-            </div>
+// Footer component for footer section
+const Footer = () => {
+  return (
+    <div className="footer">
+      Created By
+      <i class="fa-solid fa-heart"></i>
+      <a href="https://www.linkedin.com/in/chetannada/" target="_blank">
+        Chetan Nada
+      </a>
+      <i class="fa-solid fa-copyright"></i>2023
+      <strong>
+        Food<span>Fire</span>
+      </strong>
+    </div>
+  );
+};
 
-           </div>
+// AppLayout component to show: Header, Body, Footer
+const AppLayout = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <Body />
+      <Footer />
+    </React.Fragment>
+  );
+};
 
-
-    );
-  };
-
-
-    const AppLayout = () => {
-        return (
-          <div className="app">
-            
-        <Header />
-        <Body />
-        <resObj />
-      
-            </div>
-            
-        );
-      };
-      
-      const root= ReactDOM.createRoot(document.getElementById("root"));
-
-      root.render(<AppLayout />);
-      
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
